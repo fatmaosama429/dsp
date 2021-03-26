781a3a98f3b19491cbc6c745d44bc059ef323b4b
KarinAmir <61477637+KarinAmir@users.noreply.github.com>
2021-03-27 01:37:00 +0200

    add

--- view4.py
+++ view4.py
@@ -8,6 +8,8 @@ from PyQt5.uic import  loadUiType
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PyQt5.QtWidgets import QMenu
+from PyQt5.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+from PyQt5.QtCore import QAbstractEventDispatcher, QFileInfo
 
 scriptDir=dirname(realpath(__file__))
 From_Main,_= loadUiType(join(dirname(__file__),"main.ui"))
@@ -28,11 +30,47 @@ class mainwind(QMainWindow,From_Main):
         self.setMenuBar(menuBar)
         #        menubar file
         file_menu = menuBar.addMenu('file')
+        
+        open_action= QAction(QIcon("open.png"),"open File", self)
+        file_menu.addAction(open_action)
 
-        open_action = file_menu.addAction('open file')
+        saveAction= QAction(QIcon("save.png"), "save", self)
+        file_menu.addAction(saveAction)
+        saveAction.setShortcut("Ctrl+S")
+
+        printAction= QAction(QIcon("print.png"),"Print", self)
+        printAction.triggered.connect(self.printDialog)
+        file_menu.addAction(printAction)
+
+        printPreviewAction=QAction("print preview", self)
+#        printPreviewAction.triggered.connect(self.printPreviewDialog)
+        file_menu.addAction(printPreviewAction)
+        
+        pdfAction=QAction(QIcon("export.png"), "Export PDF...", self)
+        pdfAction.triggered.connect(self.pdfExport)
+        file_menu.addAction(pdfAction)
+
+        gdfAction=QAction("Export GDF...", self)
+        file_menu.addAction(gdfAction)
+
+        eventsAction=QAction("Export Events...", self)
+        file_menu.addAction(eventsAction)
+
+        importAction=QAction("Import Events...", self)
+        file_menu.addAction(importAction)
+
+        infoAction=QAction(QIcon("info.png"), "Info...", self)
+        file_menu.addAction(infoAction)
+
+        closeAction=QAction(QIcon("close.png"), "Close", self)
+        file_menu.addAction(closeAction)
+        closeAction.setShortcut("Ctrl+f4")
+
+        
         # plot_action = file_menu.addAction('plot')
-        quit= file_menu.addAction('Quit',self.close)
-        quit.setShortcut("Ctrl+Q")
+        exitAction=QAction(QIcon("exit.png"), "Exit", self)
+        file_menu.addAction(exitAction)
+        exitAction.setShortcut("Ctrl+Q")
         
         open_action.triggered.connect(self.open_sheet)
         open_action.setShortcut("Ctrl+O")
@@ -40,19 +78,126 @@ class mainwind(QMainWindow,From_Main):
         
         # menubar  edit
         edit_menu = menuBar.addMenu('edit')
-        edit_menu.addAction('Quit',self.close)
+                
+        undoAction=QAction(QIcon("undo.png"), "Undo", self)
+        edit_menu.addAction(undoAction)
+        undoAction.setShortcut("Ctrl+Z")
+
+        redoAction =QAction(QIcon("redo.png"), "Redo", self)
+        edit_menu.addAction(redoAction)
+        redoAction.setShortcut("Ctrl+Y")
+
+        toallAction =QAction(QIcon("to_all.png"), "to All Channels", self)
+        edit_menu.addAction(toallAction)
+                
+        copyAction =QAction(QIcon("copy.png"), "Copy to Channels...", self)
+        edit_menu.addAction(copyAction)
+
+        deleteAction =QAction(QIcon("delete.png"), "Delete", self)
+        edit_menu.addAction(deleteAction)
+        deleteAction.setShortcut("Del")
         
+        changeAction =QAction(QIcon("change.png"), "Change Channel...", self)
+        edit_menu.addAction(changeAction)
+
+        typeAction =QAction(QIcon("color.png"), "Change Type", self)
+        edit_menu.addAction(typeAction)
+
+        insertAction =QAction(QIcon("add.png"), "Insert Over", self)
+        edit_menu.addAction(insertAction)
+        insertAction.setShortcut("Ctrl+l")        
+
+        edit_menu.addAction('Quit',self.close)
+                
         # menubar  mode
         mode_menu = menuBar.addMenu('mode')
-        mode_menu.addAction('Quit',self.close)	
+
+        newAction =QAction(QIcon("new event.png"), "New Event", self)
+        mode_menu.addAction(newAction)
+        redoAction.setShortcut("Ctrl+1")
+
+        editAction =QAction(QIcon("edit.png"), "Edit Event", self)
+        mode_menu.addAction(editAction)
+        redoAction.setShortcut("Ctrl+2")
+
+        scrollAction =QAction(QIcon("scroll.png"), "Scroll", self)
+        mode_menu.addAction(scrollAction)
+        redoAction.setShortcut("Ctrl+3")
+
+        viewAction =QAction(QIcon("view.png"), "View Options", self)
+        mode_menu.addAction(viewAction)
+        redoAction.setShortcut("Ctrl+4")
+
         
         # menubar  view
         view_menu = menuBar.addMenu('view')
-        view_menu.addAction('Quit',self.close)
+
+        toolbarAction =QAction("Toolbars", self)
+        view_menu.addAction(toolbarAction)
+        
+        statusAction =QAction( "Statusbar", self)
+        view_menu.addAction(statusAction)
+
+        animationAction =QAction( "Animation", self)
+        view_menu.addAction(animationAction)
+
+        eveAction =QAction(QIcon("favourite.png"), "Events...", self)
+        view_menu.addAction(eveAction)
+
+        channAction =QAction(QIcon("channels.png"), "Channels...", self)
+        view_menu.addAction(channAction)
+        
+        scaleAction =QAction("Scale All", self)
+        view_menu.addAction(scaleAction)
+
+        zoominvAction =QAction(QIcon("zoomin.png"), "Zoom In Vertical", self)
+        view_menu.addAction(zoominvAction)
+        redoAction.setShortcut("Ctrl+ +")
+
+        zoomoutvAction =QAction(QIcon("zoom out.png"), "Zoom Out Vertical", self)
+        view_menu.addAction(zoomoutvAction)
+        redoAction.setShortcut("Ctrl+ -")
+
+        zoominhAction =QAction(QIcon("zoomin.png"), "Zoom In Horizontal", self)
+        view_menu.addAction(zoominhAction)
+        redoAction.setShortcut("Alt+ +")
+
+        zoomouthAction =QAction(QIcon("zoom out.png"), "Zoom Out Horizontal", self)
+        view_menu.addAction(zoomouthAction)
+        redoAction.setShortcut("Alt+ -")
+
+        gotoAction =QAction(QIcon("goto.png"), "Go to...", self)
+        view_menu.addAction(gotoAction)
+
+        gonextAction =QAction( "Goto and Select Next Event", self)
+        view_menu.addAction(gonextAction)
+        redoAction.setShortcut("Ctrl+ Right")
+
+        goprevAction =QAction("Goto and Select Previos Evenet", self)
+        view_menu.addAction(goprevAction)
+        redoAction.setShortcut("Ctrl+ Left")
+
+        fitAction =QAction("Fit View to selected Event", self)
+        view_menu.addAction(fitAction)
+
+        hideAction =QAction("Hide Events of Other Type", self)
+        view_menu.addAction(hideAction)
+
+        showAction =QAction("Show All Events", self)
+        view_menu.addAction(showAction)    
+
+ #       view_menu.addAction('Quit',self.close)
         
         # menubar  tools
         tools_menu = menuBar.addMenu('tools')
-        tools_menu.addAction('Quit',self.close)
+        
+        calcAction =QAction("Calculate Mean...", self)
+        tools_menu.addAction(calcAction)
+
+        powerAction =QAction("Power Spectrum...", self)
+        tools_menu.addAction(powerAction)
+
+#        tools_menu.addAction('Quit',self.close)
         
         # menubar  help
         help_menu = menuBar.addMenu('help')
@@ -65,12 +210,39 @@ class mainwind(QMainWindow,From_Main):
         self.toolBar= self.addToolBar('TOOL BAR')
         self.toolBar.addAction(AddFile)
         self.toolBar.setObjectName("toolBar")
-        
+
         # AddPlot = QAction(QIcon('Scatter.png'),'Scatter',self)
         # AddPlot.triggered.connect(self.Plot)
         # self.toolBar= self.addToolBar('Add plot')
         # self.toolBar.addAction(AddPlot)
+
+    def printDialog(self):
+        printer= QPrinter(QPrinter.HighResolution)
+        dialog= QPrintDialog(printer, self)
+
+        if dialog.exec_()== QPrinter.Accepted:
+            self.graphicsview.print_(printer)
+
+#    def printPreviewDialog(self):
+#        printer=QPrinter(QPrinter.HighResolution)
+#        previewDialog= QPrintPreviewDialog(printer, self)
+#        previewDialog.paintRequested.connect(self.printPreview)
+#        previewDialog.exec_()
+
+#   def printPreview(self,printer):
+#        self.graphicsview.print_(printer)
+
+    def pdfExport(self):
+        fn, _= QFileDialog.getSaveFileName(self, "Export PDF", None, "PDF files (.pdf);;All Files()" )
         
+        if fn !='':
+
+            if QFileInfo(fn).suffix() == "" :fn += '.pdf'
+
+            printer = QPrinter(QPrinter.HighResolution)
+            printer.setOutputFormat(QPrinter.PdfFormat)
+            printer.setOutputFileName(fn)
+            self.graphicsview.document().print_(printer)
 
     def open_sheet(self):
         path = QFileDialog.getOpenFileName(self, "Open", "", "CSV Files (*.csv);;All Files (*)")
